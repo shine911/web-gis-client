e9926b72fe83412c7d8ce5cccf05273ffca90c0f
maikhang <67750373+maikhang@users.noreply.github.com>
2020-07-30 16:05:55 +0700

    Update detail
    - added detail information

--- resources/lang/vi/message.php
+++ resources/lang/vi/message.php
@@ -1,23 +1,36 @@
 <?php
 //resource/lang/vi/label
 return [
-    'room_info_label' =>'Thông tin phòng học :roomname',
-    'room' => [
-        'name' => 'Tên phòng',
-        'block' => 'Block',
-        'capacity' => 'Sức chứa',
-        'department' => 'Khoa',
-        'action' => 'Tác vụ',
-    ],
-    'button' => [
-        'search' => 'Tìm kiếm',
-        'view_info' => 'Xem thông tin',
-    ],
-    'heading_sidebar' => [
-        'home' => 'Trang chủ',
-        'data_management' => 'Quản lí dữ liệu',
-        'management' => [
-            'room' => 'Quản lí dữ liệu phòng',
-        ]
-    ]
+	'room_info_label' => 'Thông tin phòng học :roomname',
+	'room' => [
+		'id' => 'Mã số',
+		'name' => 'Tên phòng',
+		'block' => 'Block',
+		'capacity' => 'Sức chứa',
+		'department' => 'Khoa',
+		'action' => 'Tác vụ',
+		'length' => 'Chiều dài',
+		'width' => 'Chiều rộng',
+		'area' => 'Chu vi',
+		'roomnamevi' => 'Tên tiếng Việt',
+		'roomnameen' => 'Tên tiếng Anh',
+		'buildingcode' => 'Khu',
+		'other1' => 'Chú thích 1',
+		'other2' => 'Chú thích 2',
+		'floor' => 'Floor',
+		'zonecode' => 'Zonecode',
+		'campuscode' => 'Campuscode',
+		'usingpurposecode' => 'Usingpurposecode',
+	],
+	'button' => [
+		'search' => 'Tìm kiếm',
+		'view_info' => 'Xem thông tin',
+	],
+	'heading_sidebar' => [
+		'home' => 'Trang chủ',
+		'data_management' => 'Quản lí dữ liệu',
+		'management' => [
+			'room' => 'Quản lí dữ liệu phòng',
+		],
+	],
 ];
--- resources/views/room/detail.blade.php
+++ resources/views/room/detail.blade.php
@@ -22,8 +22,68 @@
                 <div class="card-body">
                 <h5 class="card-title">{{ __('message.room_info_label', ["roomname"=>$room->roomcode]) }}</h5>
                     <div class="row">
-                        <div class="col-lg-6">
-                            {{__('message.room.name')}} : {{$room->roomcode}}
+                        <div class="col-lg-3">
+                            {{__('message.room.id')}}: {{$room->roomid}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.name')}}: {{$room->roomcode}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.buildingcode')}}: {{$room->buildingcode}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.usingpurposecode')}}: {{$room->usingpurposecode}}
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-lg-12 mb-2">
+                            {{__('message.room.roomnamevi')}}: {{$room->roomnamevi}}
+                        </div>
+                        <div class="col-lg-12 mb-2">
+                            {{__('message.room.roomnameen')}}: {{$room->roomnameen}}
+                        </div>
+                        <div class="col-lg-12">
+                            {{__('message.room.department')}}: {{$room->managementagencycode}}
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-lg-3">
+                            {{__('message.room.block')}}: {{$room->block}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.floor')}}: {{$room->floor}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.zonecode')}}: {{$room->zonecode}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.campuscode')}}: {{$room->campuscode}}
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-lg-3">
+                            {{__('message.room.length')}}: {{$room->length}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.width')}}: {{$room->width}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.area')}}: {{$room->area}}
+                        </div>
+                        <div class="col-lg-3">
+                            {{__('message.room.capacity')}}: {{$room->roomcapacity}}
+                        </div>
+                    </div>
+
+                    <div class="row mt-3">
+                        <div class="col-lg-12 mb-2">
+                            {{__('message.room.other1')}}: {{$room->other1}}
+                        </div>
+                        <div class="col-lg-12">
+                            {{__('message.room.other2')}}: {{$room->other2}}
                         </div>
                     </div>
 
